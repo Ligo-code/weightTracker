5b003ba38dad58d972c11a9ab3c2b4156b11a015
Ligo_code <rigatovt@gmail.com>
2025-02-05 11:58:25 -0500

    Add user registration with bcrypt password hashing and MongoDB integration

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
@@ -10,10 +11,13 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
+console.log("Mongo URI:", process.env.MONGO_URI);
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
+
+  app.use("/api/users", userRoutes);
 
 // Basic route
 app.get("/", (req, res) => {
